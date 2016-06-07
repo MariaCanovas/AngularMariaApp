c3f58a9144ed38fa80a418a8d3b5cf6334dde70a
Martinez Canovas <msme@gft.com>
2016-06-07 17:21:15 +0200

    App angular María Estilo

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -7,21 +7,55 @@
         .controller('mainController', mainController);
 
 
-     function mainController($scope, $timeout){
+     function mainController($scope, $interval){
 
+     	$scope.name = "Maria Dolores";
 	   	$scope.tareas = [{text:'Aprender AngulaJS', hecho:false}];
 	    $scope.hora = new Date();
-	    $scope.name = "Maria Dolores";
+	    $scope.segundos = $scope.hora.getSeconds();
+	    $scope.minutos = $scope.hora.getMinutes();
+	    $scope.horas = $scope.hora.getHours();
 
-	    $timeout(function() {
-		 	$scope.hora = new Date();
-		 }, 1000);
+	    $interval(function() {
+			$scope.actualizarSegundo();
+		}, 1000);
 
+	    $scope.actualizarSegundo = function(){
+	    	$scope.segundos += 1;
+	    	if($scope.segundos == 60){
+	    		$scope.segundos = 0;
+	    		$scope.actualizarMinuto();
+
+	    	}
+	    	
+	    };
+	    $scope.actualizarMinuto = function(){
+	    	$scope.minutos += 1;
+	    	if($scope.minutos == 60){
+	    		$scope.minutos = 0;
+	    		$scope.actualizarHora();
+	    	}
+	    	
+	    };
+	    $scope.actualizarHora = function(){
+	    	$scope.horas += 1;
+	    	if($scope.horas == 24){
+	    		$scope.horas = 0;
+	    	}
+	    };
 	    $scope.addTarea = function() {
 	        $scope.tareas.push({text:$scope.tarea, hecho:false});
 	        $scope.tarea = "";
 	    };
-	    
+    
 	}
+	angular.module('mariaApp').filter('fix', function() {
+	    	return function(elemento){
+	    		if(elemento < 10){
+	    			return "0" + elemento;
+	    		}
+	    		return elemento;
+	    	}; 	
+	    });
 
 }());
