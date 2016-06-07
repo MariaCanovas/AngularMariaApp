622e40edf80d7656b16999619d869c49459e2591
Martinez Canovas <msme@gft.com>
2016-06-07 12:10:13 +0200

    App angular María Reloj

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -10,35 +10,18 @@
      function mainController($scope, $timeout){
 
 	   	$scope.tareas = [{text:'Aprender AngulaJS', hecho:false}];
-	    
+	    $scope.hora = new Date();
 	    $scope.name = "Maria Dolores";
+
+	    $timeout(function() {
+		 	$scope.hora = new Date();
+		 }, 1000);
 
 	    $scope.addTarea = function() {
 	        $scope.tareas.push({text:$scope.tarea, hecho:false});
 	        $scope.tarea = "";
 	    };
-	    $scope.clock = function()
-		{
-			var reloj = new Date();
-			var horas = reloj.getHours();
-			var minutos = reloj.getMinutes();
-			var segundos = reloj.getSeconds();
-
-			// Agrega un cero si .. minutos o segundos <10
-			minutos = $scope.revisarTiempo(minutos);
-			segundos = $scope.revisarTiempo(segundos);
-			document.getElementById('reloj').innerHTML = horas + ":" + minutos + ":" + segundos;
-
-
-		};
-		$scope.revisarTiempo = function(i)
-		{
-			if (i<10)
-			{
-				i="0"+i;
-			}
-			return i;
-		};
+	    
 	}
 
 }());
