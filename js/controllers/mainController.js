19714fbe4625c5aa0a1f2cfb981d2cd05d08745f
Martinez Canovas <msme@gft.com>
2016-06-08 17:32:30 +0200

    App angular María. {fix} Cambiado el scope por vm y arregladas las funciones get y set para acceder a la localstorage

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -8,71 +8,95 @@
 
 
      function mainController($scope, $interval, localStorage){
+     	var vm = $scope;
 
-     	$scope.init = function(){
-     		$scope.name = "Maria Dolores";
-		    $scope.hora = new Date();
-		    $scope.segundos = $scope.hora.getSeconds();
-		    $scope.minutos = $scope.hora.getMinutes();
-		    $scope.horas = $scope.hora.getHours();
+     	// Funciones publicas:
+     	vm.init = init;
+     	
+     	vm.actualizarReloj = actualizarReloj;
+		vm.cargarMemoria = cargarMemoria;
+		vm.actualizarSegundo = actualizarSegundo;
+		vm.actualizarMinuto = actualizarMinuto;
+		vm.actualizarHora = actualizarHora;
+		vm.addTarea = addTarea;
+		vm.setDone = setDone;
+		vm.borrar = borrar;
+
+
+     	// Arrancar
+		vm.init();
+     	//Init
+     	function init(){
+     		asignarNombre();
+     		vm.actualizarReloj();
+		    vm.cargarMemoria();
+		}
+
+		// Funciones publicas:
+		function asignarNombre(){
+			vm.name = "Maria Dolores";
+		}
+		function actualizarReloj(){
+			vm.hora = new Date();
+		    vm.segundos = vm.hora.getSeconds();
+		    vm.minutos = vm.hora.getMinutes();
+		    vm.horas = vm.hora.getHours();
 
 		    $interval(function() {
-			$scope.actualizarSegundo();
-		}, 1000);
-
-	    $scope.actualizarSegundo = function(){
-	    	$scope.segundos += 1;
-	    	if($scope.segundos == 60){
-	    		$scope.segundos = 0;
-	    		$scope.actualizarMinuto();
+				vm.actualizarSegundo();
+			}, 1000);
+		}
+	    function actualizarSegundo(){
+	    	vm.segundos += 1;
+	    	if(vm.segundos == 60){
+	    		vm.segundos = 0;
+	    		vm.actualizarMinuto();
 	    	}
-	    };
-	    $scope.actualizarMinuto = function(){
-	    	$scope.minutos += 1;
-	    	if($scope.minutos == 60){
-	    		$scope.minutos = 0;
-	    		$scope.actualizarHora();
+	    }
+	    function actualizarMinuto(){
+	    	vm.minutos += 1;
+	    	if(vm.minutos == 60){
+	    		vm.minutos = 0;
+	    		vm.actualizarHora();
 	    	}
-	    };
-	    $scope.actualizarHora = function(){
-	    	$scope.horas += 1;
-	    	if($scope.horas == 24){
-	    		$scope.horas = 0;
+	    }
+	   function actualizarHora(){
+	    	vm.horas += 1;
+	    	if(vm.horas == 24){
+	    		vm.horas = 0;
 	    	}
-	    };
-	    $scope.cargarMemoria();
-
-     	};
-       	$scope.cargarMemoria = function(){
-       		$scope.tareas = JSON.parse(localStorage.getAll());
-       		//$scope.tareas = localStorage.getAll();
-
-       		//console.log($scope.tareas);
-	    };
-	    $scope.posicion = 0;
-	    
-	    $scope.addTarea = function() {
+	    }
+	    function cargarMemoria(){
+       		vm.tareas = localStorage.getAll();
+	    }
+	   
+	    function addTarea() {
 	    	var task = {
-	    		text: $scope.tarea,
-	    		hecho: false
-	    	};
-	    	$scope.tareas.push(task);
-	    	localStorage.set($scope.tareas.length, JSON.stringify(task));
-	    	
-	        //localStorage.set($scope.tareas.length, task);
-	        //$scope.tareas.push(task);
-	        $scope.tarea = "";
-	    };
-	    $scope.borrar = function() {
-			var tareasAnteriores = $scope.tareas;
-			$scope.tareas = [];
+                text: vm.tarea,
+                hecho: false
+            };
+	    	localStorage.set('tareas-' + vm.tareas.length, task);
+	    	vm.tareas.push(task);
+	        vm.tarea = "";
+	    }
+
+	    function setDone(index) {
+	    	var task = vm.tareas[index];
+	    	localStorage.set('tareas-' + index, task);
+	    }
+
+	    function borrar() {
+			var tareasAnteriores = vm.tareas;
+			vm.tareas = [];
 			angular.forEach(tareasAnteriores, function(tareas){
-				if ($scope.tarea.hecho === false)
-					$scope.tareas.push(tareas);
-		});
-		localStorage.set($scope.tareas.length, JSON.stringify($scope.tareas));
-	};
+				if (vm.tarea.hecho === false)
+					vm.tareas.push(tareas);
+			});
+
+			localStorage.set(vm.tareas.length, angular.toJson(vm.tareas));
+		}
 	}
+
 	angular.module('mariaApp').filter('fix', function() {
 	    	return function(elemento){
 	    		if(elemento < 10){
