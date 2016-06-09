4560a251ecb8a2df811236c8e530e851aa86290d
Martinez Canovas <msme@gft.com>
2016-06-09 09:50:45 +0200

    App angular María. {feature} Añadida la directiva para realizar la función de actualizar el reloj y mostrarlo

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # AngularMariaApp
+
+This is a simple ToDo List using angularJs. It is possible to add new tasks to the list and save it in the local storage.
+
+## Prerequisites for Windows
+
+* Chrome: https://www.google.es/chrome/browser/desktop/
+* Git: https://git-scm.com/downloads
+
+## Getting Started
+
+Just type these commands in the Git Bash console one by one.
+
+```
+git clone
+
+## Gulp tasks
+
+## Run the application
+
+## Developing
+
+## Testing
+
+## Authors
+
+- María Dolores Martínez
--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -7,17 +7,13 @@
         .controller('mainController', mainController);
 
 
-     function mainController($scope, $interval, localStorage){
+     function mainController($scope, localStorage){
      	var vm = $scope;
 
      	// Funciones publicas:
      	vm.init = init;
+     	vm.cargarMemoria = cargarMemoria;
      	
-     	vm.actualizarReloj = actualizarReloj;
-		vm.cargarMemoria = cargarMemoria;
-		vm.actualizarSegundo = actualizarSegundo;
-		vm.actualizarMinuto = actualizarMinuto;
-		vm.actualizarHora = actualizarHora;
 		vm.addTarea = addTarea;
 		vm.setDone = setDone;
 		vm.borrar = borrar;
@@ -28,7 +24,6 @@
      	//Init
      	function init(){
      		asignarNombre();
-     		vm.actualizarReloj();
 		    vm.cargarMemoria();
 		}
 
@@ -36,36 +31,7 @@
 		function asignarNombre(){
 			vm.name = "Maria Dolores";
 		}
-		function actualizarReloj(){
-			vm.hora = new Date();
-		    vm.segundos = vm.hora.getSeconds();
-		    vm.minutos = vm.hora.getMinutes();
-		    vm.horas = vm.hora.getHours();
-
-		    $interval(function() {
-				vm.actualizarSegundo();
-			}, 1000);
-		}
-	    function actualizarSegundo(){
-	    	vm.segundos += 1;
-	    	if(vm.segundos == 60){
-	    		vm.segundos = 0;
-	    		vm.actualizarMinuto();
-	    	}
-	    }
-	    function actualizarMinuto(){
-	    	vm.minutos += 1;
-	    	if(vm.minutos == 60){
-	    		vm.minutos = 0;
-	    		vm.actualizarHora();
-	    	}
-	    }
-	   function actualizarHora(){
-	    	vm.horas += 1;
-	    	if(vm.horas == 24){
-	    		vm.horas = 0;
-	    	}
-	    }
+		
 	    function cargarMemoria(){
        		vm.tareas = localStorage.getAll();
 	    }
@@ -93,7 +59,6 @@
 					vm.tareas.push(tareas);
 			});
 
-			localStorage.set(vm.tareas.length, angular.toJson(vm.tareas));
 		}
 	}
 
