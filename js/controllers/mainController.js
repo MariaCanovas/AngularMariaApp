57d33c961d9a538eba5c9ea8e574b2f7457dcda3
Martinez Canovas <msme@gft.com>
2016-06-10 14:16:54 +0200

    fix. Directiva para mostrar lista de tareas

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -2,68 +2,37 @@
 (function () {
     'use strict';
 
-    var moduleDependencies = ['mariaApp.directive'];
+    var moduleDependencies = ['mariaApp.directive', 'mariaApp.listaDirective'];
 
     angular.module('mariaApp', moduleDependencies)
         .controller('mainController', mainController);
 
-
-    function mainController($scope, localStorage){
+    function mainController($scope,localStorage){
      	var vm = $scope;
 
      	// Funciones publicas:
      	vm.init = init;
      	vm.cargarMemoria = cargarMemoria;
-     	
-     	vm.addTarea = addTarea;
-        vm.setDone = setDone;
-        vm.borrar = borrar;
-		
 
-     	// Arrancar
-		vm.init();
      	//Init
      	function init(){
      		vm.fecha = new Date();
      		asignarNombre();
-		    vm.cargarMemoria();
+     		cargarMemoria();
 		}
+
+		// Arrancar
+		vm.init();
 
 		// Funciones publicas:
 		function asignarNombre(){
 			vm.name = "Maria Dolores";
 		}
-		
-	    function cargarMemoria(){
-       		vm.tareas = localStorage.getAll();
-	    }
-	   
-	    function addTarea() {
-	    	var task = {
-                text: vm.tarea,
-                hecho: false
-            };
-	    	localStorage.set('tareas-' + vm.tareas.length, task);
-	    	vm.tareas.push(task);
-	        vm.tarea = "";
-	    }
+		function cargarMemoria(){
+            vm.tareas = localStorage.getAll();
+        }
+        
 
-	    function setDone(index) {
-	    	var task = vm.tareas[index];
-	    	localStorage.set('tareas-' + index, task);
-	    }
 
-	    function borrar(index) {
-			var tareasAnteriores = vm.tareas;
-			vm.tareas = [];
-			angular.forEach(tareasAnteriores, function(){
-				if (vm.tarea.hecho === true)
-					vm.tareas.splice(tareasAnteriores);
-			});
-			localStorage.set('tareas-' + index, tareasAnteriores);
-
-		}
 	}
-
-	
 }());
