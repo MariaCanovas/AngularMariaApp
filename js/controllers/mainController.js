fa1adfff4fd018eeee50b2c6347d2779ae9b4c49
Martinez Canovas <msme@gft.com>
2016-06-13 15:51:28 +0200

    Añadido un contador de tareas sin hacer y cambiados ligeramente los estilos

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -34,6 +34,8 @@
 		}
 		function cargarMemoria(){
             vm.tareas = localStorage.getAll();
+            console.log(vm.tareas[0].hecho);
+            toDoTasks();
         }
 
         function agregar(newId, newTask){
@@ -54,7 +56,13 @@
             cargarMemoria();
 
         }
-
-
-	}
+        function toDoTasks(){
+            vm.toDoTasks = 0;
+            for(var i = 0; i < vm.tareas.length; i++){
+                if(vm.tareas[i].hecho === false){
+                        vm.toDoTasks += 1;
+                }
+            }
+        }
+    }
 }());
